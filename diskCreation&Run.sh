29f7153ea37a2c7c7b3d0182ebd71be46354532d
Phobetore <broucewayn1413@protonmail.com>
2024-12-15 17:40:25 +0100

    correction diskcreation

--- diskCreation&Run.sh
+++ diskCreation&Run.sh
@@ -114,40 +114,41 @@ cp "$ROOTKIT_PATH" "$ROOTFS_DIR/home/user/rootkit.ko"
 chmod 700 "$ROOTFS_DIR/home/user/rootkit.ko"
 
 ########################################
-#       Configuration de GRUB et init  #
+#    Configuration de GRUB et init     #
 ########################################
 
-echo "Configuration de GRUB et init..."
+echo "Création du lien symbolique /sbin/init vers busybox..."
+ln -sf $ROOTFS_DIR/bin/busybox $ROOTFS_DIR/sbin/init
 
-# Vérifier si /sbin/init existe, sinon créer le lien symbolique
-if [ ! -f "$ROOTFS_DIR/sbin/init" ]; then
-    echo "Le fichier /sbin/init est manquant. Création du lien symbolique..."
-    sudo ln -sf /bin/busybox "$ROOTFS_DIR/sbin/init"
-fi
-
-# Vérifier la présence de busybox
-if [ ! -f "$ROOTFS_DIR/bin/busybox" ]; then
-    echo "Busybox est manquant. Téléchargement et installation..."
-    sudo cp /usr/bin/busybox "$ROOTFS_DIR/bin/"
-    sudo chmod +x "$ROOTFS_DIR/bin/busybox"
-fi
-
-# Créer la configuration GRUB
-echo "Création de la configuration GRUB..."
-sudo mkdir -p "$ROOTFS_DIR/boot/grub"
-cat <<EOF | sudo tee "$ROOTFS_DIR/boot/grub/grub.cfg"
+echo "Configuration des services essentiels..."
+sudo chroot "$ROOTFS_DIR" /bin/sh -c "
+    rc-update add devfs boot
+    rc-update add procfs boot
+    rc-update add sysfs boot
+    rc-update add root default
+    rc-update add local default
+"
+echo "Configuration de GRUB..."
+mkdir -p "$ROOTFS_DIR/boot/grub"
+cat <<EOF > "$ROOTFS_DIR/boot/grub/grub.cfg"
 serial
 terminal_input serial
 terminal_output serial
 set root=(hd0,1)
-menuentry "Linux2600" {
-    linux /boot/vmlinuz root=/dev/sda1 console=ttyS0 rw init=/sbin/init
+menuentry "Rootkit Test Environment" {
+    linux /boot/vmlinuz root=/dev/sda1 rw console=ttyS0 init=/sbin/init
 }
 EOF
 
-# Installer GRUB
-echo "Installation de GRUB..."
-sudo grub-install --directory=/usr/lib/grub/i386-pc --boot-directory="$ROOTFS_DIR/boot" "$LOOP_DEVICE"
+grub-install --directory="$GRUB_DIR" --boot-directory="$ROOTFS_DIR/boot" "$LOOP_DEVICE"
+
+echo "Ajout d'un script de démarrage pour le rootkit..."
+mkdir -p $ROOTFS_DIR/etc/local.d
+cat <<'EOF' > "$ROOTFS_DIR/etc/local.d/rootkit.start"
+#!/bin/sh
+insmod /home/user/rootkit.ko
+EOF
+chmod +x "$ROOTFS_DIR/etc/local.d/rootkit.start"
 
 
 ########################################
@@ -157,7 +158,7 @@ sudo grub-install --directory=/usr/lib/grub/i386-pc --boot-directory="$ROOTFS_DI
 echo "Démontage et finalisation..."
 umount "$ROOTFS_DIR"
 losetup -d "$LOOP_DEVICE"
-rmdir "$ROOTFS_DIR"
+rmdir $MOUNT_DIR
 
 ########################################
 #         Démarrage de la VM           #
