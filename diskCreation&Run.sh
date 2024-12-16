cca36abf3f79f69b467f607523dc5b1c0a593487
Phobetore <broucewayn1413@protonmail.com>
2024-12-16 01:56:52 +0100

    on sauve les meubles

--- diskCreation&Run.sh
+++ diskCreation&Run.sh
@@ -42,7 +42,7 @@ fi
 DISK_IMG="2600MinAlpine.img"
 DISK_SIZE="5G"
 ROOTFS_DIR="/tmp/my-rootfs"
-ALPINE_VERSION="v3.18"  # Version d'Alpine Linux à utiliser
+ALPINE_VERSION="latest" 
 LOOP_DEVICE=""
 
 cleanup() {
